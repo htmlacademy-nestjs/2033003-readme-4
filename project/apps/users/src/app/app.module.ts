a0ec06085f05ead859518aaca461dddb9a9fb656
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-07-22 15:55:44 +0300

    init publication

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { CommentModule } from './comment/comment.module';
+import { PublicationModule } from './publication/publication.module';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule, CommentModule],
+  imports: [
+    BlogUserModule,
+    AuthenticationModule,
+    CommentModule,
+    PublicationModule,
+  ],
   controllers: [],
   providers: [],
 })
